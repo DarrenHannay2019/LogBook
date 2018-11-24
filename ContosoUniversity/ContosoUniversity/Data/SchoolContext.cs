7712eb4ecf1aaf2a8816928b32acf77dce921038
Darren Hannay <42913620+DarrenHannay2019@users.noreply.github.com>
2018-11-24 20:13:41 +0000

    Completed Step 5 of the tutorial for the log book

--- ContosoUniversity/ContosoUniversity/Data/SchoolContext.cs
+++ ContosoUniversity/ContosoUniversity/Data/SchoolContext.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ContosoUniversity.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace ContosoUniversity.Models
@@ -16,5 +13,24 @@ namespace ContosoUniversity.Models
         public DbSet<Student> Student { get; set; }
 		public DbSet<Enrollment> Enrollment { get; set; }
 		public DbSet<Course> Course { get; set; }
-	}
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Department>().ToTable("Department");
+            modelBuilder.Entity<Instructor>().ToTable("Instructor");
+            modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment");
+            modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
+
+            modelBuilder.Entity<CourseAssignment>()
+                .HasKey(c => new { c.CourseID, c.InstructorID });
+        }
+
+    }
 }
